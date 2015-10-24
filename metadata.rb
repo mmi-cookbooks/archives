9ebf9638b65b60939d4a92574f95977a5007bac4
Michael Burns <michael.burns@rackspace.com>
2015-10-23 18:54:05 -0700

    bump

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'sfo-devops@lists.rackspace.com'
 license 'Apache 2.0'
 description 'adds untar_archive resource'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README'))
-version '0.0.1'
+version '1.0.0'
