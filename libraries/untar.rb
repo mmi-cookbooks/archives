1fe45c81e8954b49aa883367fe86d9650f9ae85d
Michael Burns <michael.burns@rackspace.com>
2015-11-01 01:54:47 -0700

    lint

--- libraries/untar.rb
+++ libraries/untar.rb
@@ -21,7 +21,7 @@ require 'pathname'
 
 class Chef::Resource
   class UntarArchive < Chef::Resource::File
-    def initialize(name, run_context=nil)
+    def initialize(name, run_context = nil)
       super
       @resource_name = :untar_archive
       @provider = Chef::Provider::UntarArchive
@@ -31,65 +31,64 @@ class Chef::Resource
       @nice = nil
     end
 
-    def container_path(arg=nil)
+    def container_path(arg = nil)
       set_or_return(
         :container_path,
         arg,
-        :kind_of => [String, Pathname]
+        kind_of: [String, Pathname]
       )
     end
 
-    def creates(arg=nil)
+    def creates(arg = nil)
       set_or_return(
         :creates,
         arg,
-        :kind_of => [String, Pathname]
+        kind_of: [String, Pathname]
       )
     end
 
-    def delete_dir_in_container(arg=nil)
+    def delete_dir_in_container(arg = nil)
       set_or_return(
         :delete_dir_in_container,
         arg,
-        :kind_of => [String, NilClass]
+        kind_of: [String, NilClass]
       )
     end
 
-    def nice(arg=nil)
-      arg = arg.kind_of?(Numeric) ? arg.to_i : arg
+    def nice(arg = nil)
+      arg = arg.is_a?(Numeric) ? arg.to_i : arg
       set_or_return(
         :nice,
         arg,
-        :callbacks => {
-          "not in valid numeric range. must be an integer between -19 and 19" => lambda { |m|
+        callbacks: {
+          'not in valid numeric range. must be an integer between -19 and 19' => lambda do |m|
             if m.nil?
               true
             else
-              m.kind_of?(Integer) && m >= -19 && m <= 19
+              m.is_a?(Integer) && m >= -19 && m <= 19
             end
-          }
+          end
         }
       )
     end
   end
 end
 
-
 class Chef::Provider
   class UntarArchive < Chef::Provider::File
     def action_create
-      raise "archive missing!" unless ::File.exist? @new_resource.path
+      fail 'archive missing!' unless ::File.exist? @new_resource.path
 
       createsPath = new_resource.creates.to_s.start_with?('/') ? new_resource.creates.to_s : ::File.join(new_resource.container_path, new_resource.creates)
 
-      #extract block
-      if not ::File.exists?(createsPath) or ::File.mtime(@new_resource.path) > ::File.mtime(createsPath)
-        #TODO: ensure container path exists!
-        #delete block
+      # extract block
+      if !::File.exist?(createsPath) || ::File.mtime(@new_resource.path) > ::File.mtime(createsPath)
+        # TODO: ensure container path exists!
+        # delete block
         to_remove = "#{new_resource.container_path}/#{new_resource.delete_dir_in_container}"
-        if not new_resource.delete_dir_in_container.nil? and ::File.exists?(to_remove)
-            Chef::Log.info("deleting dir_in_container: #{to_remove}")
-            FileUtils.remove_dir( to_remove, true )
+        if !new_resource.delete_dir_in_container.nil? && ::File.exist?(to_remove)
+          Chef::Log.info("deleting dir_in_container: #{to_remove}")
+          FileUtils.remove_dir(to_remove, true)
         end
         Chef::Log.info("untaring archive #{@new_resource.path} to #{new_resource.container_path}")
 
@@ -104,16 +103,16 @@ class Chef::Provider
         else
           nicevar = "nice -n #{@new_resource.nice} -- "
         end
-        #TODO: support --no-same-permissions and custom umask
-        args = { :command => "#{nicevar}tar #{decompressvar}xf #{@new_resource.path} --no-same-owner --touch -C #{new_resource.container_path}" }
+        # TODO: support --no-same-permissions and custom umask
+        args = { command: "#{nicevar}tar #{decompressvar}xf #{@new_resource.path} --no-same-owner --touch -C #{new_resource.container_path}" }
         args[:user] = @new_resource.owner if @new_resource.owner
         args[:group] = @new_resource.group if @new_resource.group
 
         begin
-            Chef::Mixin::Command.run_command(args)
-        rescue Chef::Exceptions::Exec => e
-            ::File.rename( @new_resource.path, "#{@new_resource.path}.failed" )
-            raise e
+          Chef::Mixin::Command.run_command(args)
+      rescue Chef::Exceptions::Exec => e
+        ::File.rename(@new_resource.path, "#{@new_resource.path}.failed")
+        raise e
         end
         FileUtils.touch createsPath
         new_resource.updated_by_last_action(true)
@@ -124,4 +123,4 @@ class Chef::Provider
   end
 end
 
-Chef::Platform.platforms[:default].merge! :untar_archive => Chef::Provider::UntarArchive
+Chef::Platform.platforms[:default].merge! untar_archive: Chef::Provider::UntarArchive
